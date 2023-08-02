da75798912bc527dbccb67416110451c04fe67b5
Lui-sroca <luisroca647@gmail.com>
2023-08-02 10:13:43 -0500

    Cambios realizados

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,8 +1,43 @@
 function undir() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  } 
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/*function undirLupaNavegacion(){
+  let counter = 0
+  counter++;
+  if(counter == 1){
+    const divPadre = document.querySelector(".lupa-nav")
+    const elementInput = document.createElement("input")
+ 
+  elementInput.className = "icon-nav-input"
+  elementInput.placeholder ="Buscar"
+  elementInput.style.width = "200px"
+  elementInput.style.height = "30px"
+  elementInput.style.borderRadius = "15px"
+  elementInput.style.borderStyle = "none"
+  elementInput.style.textAlign = "center"
+
+  divPadre.appendChild(elementInput)
+  } else if(counter == 2){
+
+    
+}
+  }*/
+
+function undirLupaNavegacion() {
+  const divPadre = document.querySelector(".lupa-nav");
+  const elementInput = document.createElement("input");
+  elementInput.className = "icon-nav-input";
+  elementInput.placeholder = "Buscar";
+  elementInput.style.width = "200px";
+  elementInput.style.height = "30px";
+  elementInput.style.borderRadius = "15px";
+  elementInput.style.borderStyle = "none";
+  elementInput.style.textAlign = "center";
+  divPadre.appendChild(elementInput);
+}
