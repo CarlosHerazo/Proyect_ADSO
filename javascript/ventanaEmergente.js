33ab9388be68eb67308ed406eb85d9a8d5f7693b
CarlosHerazo <102492748+CarlosHerazo@users.noreply.github.com>
2023-08-05 15:13:08 -0500

    Realizacion de ventanaEmergente

---
+++ javascript/ventanaEmergente.js
@@ -0,0 +1,13 @@
+// Obtener todos los contenedores de imágenes
+const imagenContainers = document.querySelectorAll('.imagen__producto-container');
+
+// Agregar eventos para mostrar y ocultar la descripción emergente
+imagenContainers.forEach((container) => {
+    let descripcionEmergente = container.querySelector('.descripcion-emergente');
+    container.onmouseover = () => {
+        descripcionEmergente.style.display = 'block';
+    };
+    container.onmouseout = () => {
+        descripcionEmergente.style.display = 'none';
+    };
+});
