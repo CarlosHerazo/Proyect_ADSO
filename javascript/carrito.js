747750ff45e13cb14161a3501d74960285a6ad80
CarlosHerazo <102492748+CarlosHerazo@users.noreply.github.com>
2023-08-20 22:27:38 -0500

    cambios realizados en el carrito

--- javascript/carrito.js
+++ javascript/carrito.js
@@ -59,7 +59,7 @@ function carrito(){
     let img = document.createElement("p")
 
     img.id=i
-    img.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" id='+i+' onclick = borrar(this.id) height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#}</style><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"  /></svg>'
+    img.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" id='+i+' class="eliminar" onclick = borrar(this.id) height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>.eliminar{fill:#e57373}</style><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"  /></svg>'
     div2.setAttribute("id", i)  
     div2.className = "delete" 
     texto.innerHTML = titulo
@@ -81,7 +81,32 @@ function carrito(){
 }
 function borrar(botonid){
     productosagregados.splice(botonid, 1)
+    totalClicks--
+    contador.textContent = totalClicks;
+    ContenedorCarrito.appendChild(contador);
+
+
+    // Selecciona el elemento padre que contiene los productos agregados
+    let contenedorCarrito = document.getElementById("lista-carrito");
+    let divPadreCarrito = document.getElementById("contenido-carrito");
+
+    // Intenta encontrar el elemento hijo que coincide con el ID proporcionado por el parámetro
+    let productoAQuitar = document.getElementById(botonid);
+
+    // Verifica si se encontró el elemento antes de intentar eliminarlo
+    if (productoAQuitar) {
+        // Si hay solo un hijo en el contenedor, elimina también el contenedor
+        if (productoAQuitar) {
+            // Si hay solo un hijo en el contenedor, elimina también el contenedor
+            if (contenedorCarrito.children.length === 1) {
+                divPadreCarrito.parentNode.removeChild(divPadreCarrito);
+            } else {
+                contenedorCarrito.removeChild(productoAQuitar);
+            }
+    }
 }
+}
+
 
 
 
--- javascript/onclickProductos.js
+++ javascript/onclickProductos.js
@@ -26,20 +26,37 @@ document.getElementById("Producto6").addEventListener("click", function(){
 
 
 
-document.getElementById("Producto1").addEventListener("click", yuca)
-document.getElementById("Producto2").addEventListener("click", Platano)
-document.getElementById("Producto3").addEventListener("click", auyama)
-document.getElementById("Producto4").addEventListener("click", limon)
-document.getElementById("Producto5").addEventListener("click", ñame)
-document.getElementById("Producto6").addEventListener("click", guayaba)
+document.querySelectorAll("#Producto1").forEach(informacion => {
+    informacion.addEventListener("click", yuca);
+});
+
+document.querySelectorAll("#Producto2").forEach(informacion => {
+    informacion.addEventListener("click", Platano);
+})
+document.querySelectorAll("#Producto3").forEach(informacion => {
+    informacion.addEventListener("click", auyama);
+})
+
+document.querySelectorAll("#Producto4").forEach(informacion => {
+    informacion.addEventListener("click", limon);
+})
+
+document.querySelectorAll("#Producto5").forEach(informacion => {
+    informacion.addEventListener("click", ñame);
+})
+
+document.querySelectorAll("#Producto6").forEach(informacion => {
+    informacion.addEventListener("click", guayaba);
+})
+
 
 
 function yuca(){
-    let id = document.getElementsByClassName("producto-info")
-    let titulo = document.getElementById("titulo-info-producto")
-    let imagen = document.getElementById("producto-info")
-    let textoInfo = document.getElementById("Texto-info-producto")
-    let precio = document.getElementById("precio-producto")
+    let id = document.getElementsByClassName("producto-info");
+    let titulo = document.getElementById("titulo-info-producto");
+    let imagen = document.getElementById("producto-info");
+    let textoInfo = document.getElementById("Texto-info-producto");
+    let precio = document.getElementById("precio-producto");
 
     id.id = "producto1"
     titulo.innerHTML="Yuca"
@@ -54,11 +71,11 @@ function yuca(){
 }
 
 function Platano(){
-    let id = document.getElementsByClassName("producto-info")
-    let titulo = document.getElementById("titulo-info-producto")
-     let imagen = document.getElementById("producto-info")
-     let textoInfo = document.getElementById("Texto-info-producto")
-     let precio = document.getElementById("precio-producto")
+    let id = document.getElementsByClassName("producto-info");
+    let titulo = document.getElementById("titulo-info-producto");
+     let imagen = document.getElementById("producto-info");
+     let textoInfo = document.getElementById("Texto-info-producto");
+     let precio = document.getElementById("precio-producto");
      
      id.id = "producto2"
      titulo.innerHTML="Plátano verde"
@@ -71,11 +88,11 @@ function Platano(){
      precio.style.marginBottom = "60px"
  }
  function auyama(){
-    let id = document.getElementsByClassName("producto-info")
-    let titulo = document.getElementById("titulo-info-producto")
-     let imagen = document.getElementById("producto-info")
-     let textoInfo = document.getElementById("Texto-info-producto")
-     let precio = document.getElementById("precio-producto")
+    let id = document.getElementsByClassName("producto-info");
+    let titulo = document.getElementById("titulo-info-producto");
+     let imagen = document.getElementById("producto-info");
+     let textoInfo = document.getElementById("Texto-info-producto");
+     let precio = document.getElementById("precio-producto");
 
      id.id = "producto3"
      titulo.innerHTML="Auyama"
@@ -83,15 +100,15 @@ function Platano(){
      textoInfo.innerHTML= "La auyama o ahuyama (ambas formas son correctas según la Real Academia de la Lengua Española-RAE) es uno de los alimentos con múltiples beneficios para el organismo. Este vegetal contiene calcio, sodio, magnesio, zinc, hierro, potasio, fósforo, vitaminas A, C y B."
      precio.innerHTML = "Precio por Unidad: $400 - Aplica para personas de la misma localidad <br> Precio por Bulto: $450"
  
-     textoInfo.style.marginTop = "50px"
-     precio.style.marginTop = "40px"
-     precio.style.marginBottom = "60px"
+     textoInfo.style.marginTop = "50px";
+     precio.style.marginTop = "40px";
+     precio.style.marginBottom = "60px";
  }
  function limon(){
-    let id = document.getElementsByClassName("producto-info")
-    let titulo = document.getElementById("titulo-info-producto")
-     let imagen = document.getElementById("producto-info")
-     let textoInfo = document.getElementById("Texto-info-producto")
+    let id = document.getElementsByClassName("producto-info");
+    let titulo = document.getElementById("titulo-info-producto");
+     let imagen = document.getElementById("producto-info");
+     let textoInfo = document.getElementById("Texto-info-producto");
      let precio = document.getElementById("precio-producto")
 
      id.id = "producto4"
@@ -100,42 +117,42 @@ function Platano(){
      textoInfo.innerHTML="El limón es redondo y ligeramente alargado, pertenece a la familia de los agrios y por tanto comparte muchas de las características de otras especies de cítricos, como es tener una piel gruesa. La pulpa es color amarillo pálido, jugosa y de sabor ácido dividida en gajos. El color de la corteza es amarillo y especialmente brillante cuando está maduro."
      precio.innerHTML = "Precio por Unidad: $400 - Aplica para personas de la misma localidad <br> Precio por Bulto: $450"
  
-     textoInfo.style.marginTop = "50px"
-     precio.style.marginTop = "40px"
-     precio.style.marginBottom = "60px"
+     textoInfo.style.marginTop = "50px";
+     precio.style.marginTop = "40px";
+     precio.style.marginBottom = "60px";
  }
  function ñame(){
-    let id = document.getElementsByClassName("producto-info")
-    let titulo = document.getElementById("titulo-info-producto")
-     let imagen = document.getElementById("producto-info")
-     let textoInfo = document.getElementById("Texto-info-producto")
-     let precio = document.getElementById("precio-producto")
+    let id = document.getElementsByClassName("producto-info");
+    let titulo = document.getElementById("titulo-info-producto");
+     let imagen = document.getElementById("producto-info");
+     let textoInfo = document.getElementById("Texto-info-producto");
+     let precio = document.getElementById("precio-producto");
 
-     id.id = "producto5"
-     titulo.innerHTML="Ñame"
-     imagen.src = "/img/Productos/vegetales-dieta_vegetariana-nutricion_421718519_132322710_1706x960.jpg"
+     id.id = "producto5";
+     titulo.innerHTML="Ñame";
+     imagen.src = "/img/Productos/vegetales-dieta_vegetariana-nutricion_421718519_132322710_1706x960.jpg";
      textoInfo.innerHTML="Los ñames son tubérculos almidonados de origen africano que son un alimento básico en América del Sur, África, las Antillas y las Islas del Pacífico. Además, es un alimento popular en las Islas Canarias, donde además existen plantaciones. A pesar de su contenido en almidón, el ñame tiene un bajo índice glucémico pues aporta carbohidratos complejos y fibra dietética."
      precio.innerHTML = "Precio por Unidad: $400 - Aplica para personas de la misma localidad <br> Precio por Bulto: $450"
  
-     textoInfo.style.marginTop = "50px"
-     precio.style.marginTop = "40px"
-     precio.style.marginBottom = "60px"
+     textoInfo.style.marginTop = "50px";
+     precio.style.marginTop = "40px";
+     precio.style.marginBottom = "60px";
  }
  function guayaba(){
-    let id = document.getElementsByClassName("producto-info")
-    let titulo = document.getElementById("titulo-info-producto")
-     let imagen = document.getElementById("producto-info")
-     let textoInfo = document.getElementById("Texto-info-producto")
-     let precio = document.getElementById("precio-producto")
+    let id = document.getElementsByClassName("producto-info");
+    let titulo = document.getElementById("titulo-info-producto");
+     let imagen = document.getElementById("producto-info");
+     let textoInfo = document.getElementById("Texto-info-producto");
+     let precio = document.getElementById("precio-producto");
 
-     id.id = "producto6"
-     titulo.innerHTML="Guayaba"
-     imagen.src = "/img/Productos/NUTE7EOPZVG4TBLG5EXKA5GAIA.avif"
-     textoInfo.innerHTML="Con el nombre científico de Psydium guajava se conoce el fruto del árbol guayabo. Este es originario de México, Brasil y el Perú; pero hoy en día se cultiva en países de clima tropical alrededor del mundo. Por este motivo, en zonas como América Central, India, Bangladés, Indonesia o China es muy conocida y consumida de forma habitual. La planta no solo se valora como alimento, sino que también cuenta con una larga historia de usos tradicionales como remedio natural para diversas condiciones de salud."
-     precio.innerHTML = "Precio por Unidad: $400 - Aplica para personas de la misma localidad <br> Precio por Bulto: $450"
+     id.id = "producto6";
+     titulo.innerHTML="Guayaba";
+     imagen.src = "/img/Productos/NUTE7EOPZVG4TBLG5EXKA5GAIA.avif";
+     textoInfo.innerHTML="Con el nombre científico de Psydium guajava se conoce el fruto del árbol guayabo. Este es originario de México, Brasil y el Perú; pero hoy en día se cultiva en países de clima tropical alrededor del mundo. Por este motivo, en zonas como América Central, India, Bangladés, Indonesia o China es muy conocida y consumida de forma habitual. La planta no solo se valora como alimento, sino que también cuenta con una larga historia de usos tradicionales como remedio natural para diversas condiciones de salud.";
+     precio.innerHTML = "Precio por Unidad: $400 - Aplica para personas de la misma localidad <br> Precio por Bulto: $450";
  
-     textoInfo.style.marginTop = "50px"
-     precio.style.marginTop = "40px"
-     precio.style.marginBottom = "60px"
+     textoInfo.style.marginTop = "50px";
+     precio.style.marginTop = "40px";
+     precio.style.marginBottom = "60px";
  }
 
