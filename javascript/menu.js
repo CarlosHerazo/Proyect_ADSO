e72a8cf1b80c5d647c545492af37339b53864771
CarlosHerazo <102492748+CarlosHerazo@users.noreply.github.com>
2023-09-02 23:16:11 -0500

    Actualizacion Header

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,13 +1,3 @@
-function undir() {
-  let menuResponsive = document.querySelector('.menu__responsive');
-  menuResponsive.style.display = "block";
-}
-
-let iconoquitar = document.getElementById("iconoQuitar");
-let menuResponsive = document.querySelector('.menu__responsive');
-iconoquitar.addEventListener("click", ()=>{
-  menuResponsive.style.display = "none";
-})
 
 /*function undirLupaNavegacion(){
   let counter = 0
@@ -52,32 +42,32 @@ function undirLupaNavegacion()
 */
 
 
-document.getElementById("icon-nav-lupa").addEventListener("click", lupa)
-document.getElementById("icon-nav-lupa").addEventListener("click", lupa2);
+// document.getElementById("icon-nav-lupa").addEventListener("click", lupa)
+// document.getElementById("icon-nav-lupa").addEventListener("click", lupa2);
 
 
-function lupa(borra){
+// function lupa(borra){
 
-  borra.target.removeEventListener(borra.type, lupa);
+//   borra.target.removeEventListener(borra.type, lupa);
 
-  const divPadre = document.getElementById("lupa-nav");
-  const elementInput = document.createElement("input");
-  elementInput.id = "input-lupa";
-  elementInput.placeholder = "Buscar";
-  elementInput.style.width = "200px";
-  elementInput.style.height = "30px";
-  elementInput.style.borderRadius = "15px";
-  elementInput.style.borderStyle = "none";
-  elementInput.style.textAlign = "center";
+//   const divPadre = document.getElementById("lupa-nav");
+//   const elementInput = document.createElement("input");
+//   elementInput.id = "input-lupa";
+//   elementInput.placeholder = "Buscar";
+//   elementInput.style.width = "200px";
+//   elementInput.style.height = "30px";
+//   elementInput.style.borderRadius = "15px";
+//   elementInput.style.borderStyle = "none";
+//   elementInput.style.textAlign = "center";
 
-  divPadre.appendChild(elementInput);
-}
+//   divPadre.appendChild(elementInput);
+// }
 
-function lupa2(){
+// function lupa2(){
 
-  document.getElementById("icon-nav-lupa").addEventListener("click", lupa)
-  const divPadre = document.getElementById("lupa-nav");
-  const elementInput = document.getElementById("input-lupa");
+//   document.getElementById("icon-nav-lupa").addEventListener("click", lupa)
+//   const divPadre = document.getElementById("lupa-nav");
+//   const elementInput = document.getElementById("input-lupa");
 
-  divPadre.removeChild(elementInput)
-}
+//   divPadre.removeChild(elementInput)
+// }
