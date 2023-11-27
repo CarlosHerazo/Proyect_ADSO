bf1afaa44d355155b456e3368d2bddedab1b9b50
CarlosHerazo <Carlosherazo2004@gmail.com>
2023-11-27 01:29:49 -0500

    diseño de las cards

--- Admin_System/vistas/modulos/pagina_en_blanco.php
+++ Admin_System/vistas/modulos/pagina_en_blanco.php
@@ -3,9 +3,9 @@
     <div class="container-fluid">
 
         <div class="row mb-2">
-            
+
             <div class="col-sm-6">
-                <h1>Pagina en Blanco</h1>
+                <h1>Estadisticas de Compras</h1>
             </div>
 
             <div class="col-sm-6">
@@ -14,7 +14,7 @@
                         <a href="#">Inicio</a>
                     </li>
                     <li class="breadcrumb-item">
-                        Pagina en Blanco
+                        Estadisticas de Compras
                     </li>
                 </ol>
             </div>
@@ -23,6 +23,28 @@
 
 </section>
 
-<section class="content">
+<section class="container">
+    <div class="row justify-content-between">
+        <div class="card text-white bg-danger text-center" style="width: 18rem;">
+            <div class="card-header">Total Ventas</div>
+            <div class="card-body">
+            <p class="card-title text-center">$100,000</p>
+                
+            </div>
+        </div>
+        <div class="card text-white bg-warning text-center" style="width: 18rem;">
+            <div class="card-header">Empleados Administrativos</div>
+            <div class="card-body">
+                <h2 class="card-title text-center">10</h2>
+                
+            </div>
+        </div>
+        <div class="card text-white bg-success" style="width: 18rem;">
+            <div class="card-header text-center">Productos en Stock</div>
+            <div class="card-body">
+            <h2 class="card-title text-center">100</h2>
+            </div>
+        </div>
 
+    </div>
 </section>
