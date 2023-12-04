6a76f2af1b9e2842864127ce31fb78ea4f1d667d
CarlosHerazo <Carlosherazo2004@gmail.com>
2023-12-03 22:39:29 -0500

    chekaout

--- page/pago.php
+++ page/pago.php
@@ -78,7 +78,7 @@ if ($_POST) {
         <h1 class="display-4">¡Paso Final!</h1>
         <hr class="my-2">
         <p class="lead">Estas a punto de pagar la cantidad de:
-        <h4 id="totalPagar"data-total="<?php echo number_format($total, 2); ?>">$<?php echo number_format($total, 2) ?></h4>
+        <h4>$<?php echo number_format($total, 2) ?></h4>
         </p>
 
         <p class="lead">
@@ -157,7 +157,66 @@ if ($_POST) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://www.paypal.com/sdk/js?client-id=<?php echo CLIENT_ID; ?>&currency=<?php echo CURRENCY; ?>"></script>
 
-    <script src="../javascript/pago_paypal.js"></script>
+    <script>
+        paypal.Buttons({
+            style: {
+                color: 'blue',
+                shape: 'pill',
+                label: 'pay'
+            },
+            createOrder: function(data, actions) {
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: {
+                            value: <?php echo $total; ?>
+                        }
+                    }]
+                });
+            },
+
+            onApprove: function(data, actions) {
+                actions.order.capture().then(function(detalles) {
+                    if (data) {
+                        let url = '../checkout/captura_paypal.php';
+                        fetch(url, {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({
+                                    detalles: detalles
+                                })
+                            })
+                            .then(response => response.json())
+                            .then(data => {
+                                // Manejar la respuesta del servidor
+                                console.log(data);
+
+                                Swal.fire({
+                                    title: "El Pago fue exitoso!",
+                                    text: "Gracias por su compra",
+                                    icon: "success"
+                                });
+                            })
+                            .catch(error => {
+                                // Manejar errores de la solicitud
+                                console.error('Error en la solicitud:', error);
+                            });
+                    }
+                });
+            },
+
+
+            onCancel: function(data) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "TU PAGO FUE CANCELADO!",
+                    footer: '<a href="../index.php">Regrese al inicio</a>'
+                })
+            }
+        }).render('#paypal-button-container')
+    </script>
 </body>
 
 </html>
