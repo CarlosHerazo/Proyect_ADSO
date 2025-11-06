dba114f5c8ffd5cb0a3f80064eeae4c6d3b70ee2
CarlosHerazo <Carlosherazo2004@gmail.com>
2025-11-05 21:09:34 -0500

    aaaa

--- Admin_System/modelo/Conexion.php
+++ Admin_System/modelo/Conexion.php
@@ -2,11 +2,9 @@
     class Conexion{
 
         static public  function conectar(){
-
             $con = new PDO("mysql:host=db;dbname=myapp","myuser","mypassword",
             array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                   PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-        
           return $con;
         }
       
